7f21c862bc1a8e2481d8af5f3405babce502c2d1
ManuelJBM <manbenmer1@alum.us.es>
2024-05-09 17:48:08 +0200

    Testeos con JUnit

--- src/test/java/aiss/grupo6/dailymotionMiner/DailymotionMinerApplicationTests.java
+++ src/test/java/aiss/grupo6/dailymotionMiner/DailymotionMinerApplicationTests.java
@@ -1,10 +1,21 @@
 package aiss.grupo6.dailymotionMiner;
 
+import aiss.grupo6.dailymotionMiner.service.CaptionServiceTest;
+import aiss.grupo6.dailymotionMiner.service.ChannelServiceTest;
+import aiss.grupo6.dailymotionMiner.service.VideoServiceTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DailymotionMinerApplicationTests {
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		ChannelServiceTest.class,
+		VideoServiceTest.class,
+		CaptionServiceTest.class
+})
+public class DailymotionMinerApplicationTests {
 
 	@Test
 	void contextLoads() {
